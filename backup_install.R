789d3311ea69195b852631d241581102c20a545a
Adam Garber <garberadamc@gmail.com>
2025-11-19 09:56:58 -0800

    Initial commit

---
+++ backup_install.R
@@ -0,0 +1,11 @@
+
+# BACKUP: Install packages
+
+install.packages("tidyverse")
+install.packages("here")
+install.packages("janitor")
+install.packages("gt")
+install.packages("gtsummary")
+install.packages("gtExtras")
+install.packages("praise")
+install.packages("beepr") 
